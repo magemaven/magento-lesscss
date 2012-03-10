d49227a25cd8d7433359fa7187a7576c9105ca22
Sergey Storchay <r8@r8.com.ua>
2012-03-10 15:10:45 +0200

    Created module structure

---
+++ app/code/community/Magemaven/Lesscss/Helper/Data.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE_AFL.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ *
+ * @category    Magemaven
+ * @package     Magemaven_Lesscss
+ * @copyright   Copyright (c) 2012 Sergey Storchay <r8@r8.com.ua>
+ * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
+class Magemaven_Lesscss_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
