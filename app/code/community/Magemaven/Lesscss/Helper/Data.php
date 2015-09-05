cd2ff613f231450a8d3ad55448c9fa5fda60afe1
Sergey Storchay <r8@r8.com.ua>
2015-09-05 05:46:03 -0400

    Applied fixes from StyleCI

--- app/code/community/Magemaven/Lesscss/Helper/Data.php
+++ app/code/community/Magemaven/Lesscss/Helper/Data.php
@@ -1,23 +1,25 @@
 <?php
+
 /**
  * This source file is subject to the Academic Free License (AFL 3.0)
  * that is bundled with this package in the file LICENSE_AFL.txt.
  * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/afl-3.0.php
+ * http://opensource.org/licenses/afl-3.0.php.
  *
  * @category    Magemaven
- * @package     Magemaven_Lesscss
+ *
  * @copyright   Copyright (c) 2012 Sergey Storchay <r8@r8.com.ua>
  * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
-require_once(Mage::getBaseDir('lib') . DS . 'lessphp' . DS .'lessc.inc.php');
+require_once Mage::getBaseDir('lib').DS.'lessphp'.DS.'lessc.inc.php';
 
 class Magemaven_Lesscss_Helper_Data extends Mage_Core_Helper_Abstract
 {
     /**
-     * Get file extension in lower case
+     * Get file extension in lower case.
      *
      * @param $file
+     *
      * @return string
      */
     public function getFileExtension($file)
@@ -26,9 +28,10 @@ class Magemaven_Lesscss_Helper_Data extends Mage_Core_Helper_Abstract
     }
 
     /**
-     * Compile less file and return full path to created css
+     * Compile less file and return full path to created css.
      *
      * @param $file
+     *
      * @return string
      */
     public function compile($file)
@@ -39,8 +42,8 @@ class Magemaven_Lesscss_Helper_Data extends Mage_Core_Helper_Abstract
 
         try {
             $targetFilename = Mage::getBaseDir('media')
-                . DS . 'lesscss' . DS . md5($file) . '.css';
-            $cacheKey = 'less_' . $file;
+                .DS.'lesscss'.DS.md5($file).'.css';
+            $cacheKey = 'less_'.$file;
 
             /** @var $cacheModel Mage_Core_Model_Cache */
             $cacheModel = $cache = Mage::getSingleton('core/cache');
@@ -64,7 +67,6 @@ class Magemaven_Lesscss_Helper_Data extends Mage_Core_Helper_Abstract
                 file_put_contents($targetFilename, $cache['compiled']);
                 $cacheModel->save(serialize($cache), $cacheKey);
             }
-
         } catch (Exception $e) {
             Mage::logException($e);
             $targetFilename = '';
